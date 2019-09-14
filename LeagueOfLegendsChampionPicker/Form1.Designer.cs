b8c15f889313e5b8393f010584ffcff98ac8bf87
Daniel Roldan <danidjrm@hotmail.com>
2019-09-13 22:04:59 -0500

    First Commit

---
+++ LeagueOfLegendsChampionPicker/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace LeagueOfLegendsChampionPicker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChampionPicker = new System.Windows.Forms.Button();
+            this.lblChampionPicked = new System.Windows.Forms.Label();
+            this.picChampionPic = new System.Windows.Forms.PictureBox();
+            this.tabControls = new System.Windows.Forms.TabControl();
+            this.tabPickAChampion = new System.Windows.Forms.TabPage();
+            this.grpboxYourChampionPool = new System.Windows.Forms.GroupBox();
+            this.tabChampionPool = new System.Windows.Forms.TabPage();
+            this.grpboxChampionPool = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picChampionPic)).BeginInit();
+            this.tabControls.SuspendLayout();
+            this.tabPickAChampion.SuspendLayout();
+            this.tabChampionPool.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChampionPicker
+            // 
+            this.btnChampionPicker.Location = new System.Drawing.Point(401, 219);
+            this.btnChampionPicker.Name = "btnChampionPicker";
+            this.btnChampionPicker.Size = new System.Drawing.Size(152, 37);
+            this.btnChampionPicker.TabIndex = 0;
+            this.btnChampionPicker.Text = "Pick A Champion";
+            this.btnChampionPicker.UseVisualStyleBackColor = true;
+            this.btnChampionPicker.Click += new System.EventHandler(this.btnChampionPicker_Click);
+            // 
+            // lblChampionPicked
+            // 
+            this.lblChampionPicked.AutoSize = true;
+            this.lblChampionPicked.Location = new System.Drawing.Point(658, 259);
+            this.lblChampionPicked.Name = "lblChampionPicked";
+            this.lblChampionPicked.Size = new System.Drawing.Size(117, 17);
+            this.lblChampionPicked.TabIndex = 1;
+            this.lblChampionPicked.Text = "Champion Picked";
+            this.lblChampionPicked.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picChampionPic
+            // 
+            this.picChampionPic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picChampionPic.Location = new System.Drawing.Point(594, 16);
+            this.picChampionPic.Name = "picChampionPic";
+            this.picChampionPic.Size = new System.Drawing.Size(240, 240);
+            this.picChampionPic.TabIndex = 2;
+            this.picChampionPic.TabStop = false;
+            // 
+            // tabControls
+            // 
+            this.tabControls.Controls.Add(this.tabPickAChampion);
+            this.tabControls.Controls.Add(this.tabChampionPool);
+            this.tabControls.Location = new System.Drawing.Point(12, 12);
+            this.tabControls.Name = "tabControls";
+            this.tabControls.SelectedIndex = 0;
+            this.tabControls.Size = new System.Drawing.Size(881, 629);
+            this.tabControls.TabIndex = 3;
+            // 
+            // tabPickAChampion
+            // 
+            this.tabPickAChampion.AutoScroll = true;
+            this.tabPickAChampion.Controls.Add(this.grpboxYourChampionPool);
+            this.tabPickAChampion.Controls.Add(this.btnChampionPicker);
+            this.tabPickAChampion.Controls.Add(this.picChampionPic);
+            this.tabPickAChampion.Controls.Add(this.lblChampionPicked);
+            this.tabPickAChampion.Location = new System.Drawing.Point(4, 25);
+            this.tabPickAChampion.Name = "tabPickAChampion";
+            this.tabPickAChampion.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPickAChampion.Size = new System.Drawing.Size(873, 600);
+            this.tabPickAChampion.TabIndex = 0;
+            this.tabPickAChampion.Text = "Pick a Champion";
+            this.tabPickAChampion.UseVisualStyleBackColor = true;
+            // 
+            // grpboxYourChampionPool
+            // 
+            this.grpboxYourChampionPool.AutoSize = true;
+            this.grpboxYourChampionPool.Location = new System.Drawing.Point(6, 294);
+            this.grpboxYourChampionPool.Name = "grpboxYourChampionPool";
+            this.grpboxYourChampionPool.Size = new System.Drawing.Size(838, 300);
+            this.grpboxYourChampionPool.TabIndex = 3;
+            this.grpboxYourChampionPool.TabStop = false;
+            this.grpboxYourChampionPool.Text = "Your Champion Pool";
+            // 
+            // tabChampionPool
+            // 
+            this.tabChampionPool.AutoScroll = true;
+            this.tabChampionPool.Controls.Add(this.grpboxChampionPool);
+            this.tabChampionPool.Location = new System.Drawing.Point(4, 25);
+            this.tabChampionPool.Name = "tabChampionPool";
+            this.tabChampionPool.Padding = new System.Windows.Forms.Padding(3);
+            this.tabChampionPool.Size = new System.Drawing.Size(873, 600);
+            this.tabChampionPool.TabIndex = 2;
+            this.tabChampionPool.Text = "Champion Pool";
+            this.tabChampionPool.UseVisualStyleBackColor = true;
+            // 
+            // grpboxChampionPool
+            // 
+            this.grpboxChampionPool.AutoSize = true;
+            this.grpboxChampionPool.Location = new System.Drawing.Point(6, 249);
+            this.grpboxChampionPool.Name = "grpboxChampionPool";
+            this.grpboxChampionPool.Size = new System.Drawing.Size(861, 345);
+            this.grpboxChampionPool.TabIndex = 0;
+            this.grpboxChampionPool.TabStop = false;
+            this.grpboxChampionPool.Text = "Champion Pool";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 653);
+            this.Controls.Add(this.tabControls);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "League Random Champion Pick";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picChampionPic)).EndInit();
+            this.tabControls.ResumeLayout(false);
+            this.tabPickAChampion.ResumeLayout(false);
+            this.tabPickAChampion.PerformLayout();
+            this.tabChampionPool.ResumeLayout(false);
+            this.tabChampionPool.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChampionPicker;
+        private System.Windows.Forms.Label lblChampionPicked;
+        private System.Windows.Forms.PictureBox picChampionPic;
+        private System.Windows.Forms.TabControl tabControls;
+        private System.Windows.Forms.TabPage tabPickAChampion;
+        private System.Windows.Forms.GroupBox grpboxYourChampionPool;
+        private System.Windows.Forms.TabPage tabChampionPool;
+        private System.Windows.Forms.GroupBox grpboxChampionPool;
+    }
+}
+
---
+++ LeagueOfLegendsChampionPicker/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeagueOfLegendsChampionPicker
+{
+    public partial class MainForm : Form
+    {
+        private Random random = new Random();
+
+        private string champSelectedFromPool;
+        private string champSelectedFromYourPool;
+
+        //Champion Pool
+        private string championPool = System.IO.File.ReadAllText(@"..\..\obj\TextFiles\ChampionPool.txt");
+
+        private string yourChampionPool = System.IO.File.ReadAllText(@"..\..\obj\TextFiles\YourChampionPool.txt");
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] champArrayYourChampion = ChampionPickerToArray(yourChampionPool);
+            string[] champArrayChampionPool = ChampionPickerToArray(championPool);
+
+            ImageCreator(champArrayYourChampion.Length, champArrayYourChampion, grpboxYourChampionPool);
+            ImageCreator(champArrayChampionPool.Length, champArrayChampionPool, grpboxChampionPool);
+
+        }
+
+        private void btnChampionPicker_Click(object sender, EventArgs e)
+        {
+            //Saves the name of the champion picked
+            string championPicked = ChampionPickerRandomizer();
+
+            //sets the label to the champion name and to the picture
+            lblChampionPicked.Text = championPicked;
+            picChampionPic.SizeMode = PictureBoxSizeMode.StretchImage;
+            picChampionPic.Image = Image.FromFile(@"..\..\obj\Images\" + championPicked + ".png");
+        }
+
+        //Randomize the champion
+        private string ChampionPickerRandomizer()
+        {
+            string [] champArray = ChampionPickerToArray(yourChampionPool);
+            //Randomizes the champion and picks one to play
+            int championselected = random.Next(0, champArray.Length);
+
+            //Returns the name of the champion
+            return champArray[championselected];
+
+        }
+
+        //Converts the given type of pool champ to an array
+        private string [] ChampionPickerToArray(string typeOfPoolChamp)
+        {
+            //Grabs the champion pool and converts it to a list
+            List<string> champList = typeOfPoolChamp.Split(',').ToList();
+
+            //Converts list to array
+            string[] champArray = champList.ToArray();
+
+            return champArray;
+        }
+
+        private void ImageCreator(int numOfChampsInYourList, string [] champArray, GroupBox nameOfGroup )
+        {
+            //Creates the Image boxes and sets spacing in the groupbox
+            int vertMargin = 0;
+            int championPosition = 0;
+            int counter = 0;
+            for (int iVert = 0; iVert < numOfChampsInYourList; iVert++)
+            {
+                int horizMargin = 0;
+                for (int iHoriz = 0; iHoriz < 5; iHoriz++)
+                {
+                    if (counter >= numOfChampsInYourList)
+                    {
+                        break;
+                    }
+
+                    else if (champArray[championPosition] == "")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        PictureBox p = new PictureBox();
+                        p.Location = new Point(5 + (iHoriz * 5 + horizMargin), 15 + (iVert * 5 + vertMargin));
+                        p.Size = new Size(120, 120);
+                        p.BorderStyle = BorderStyle.Fixed3D;
+                        p.BackColor = Color.Black;
+                        p.Name = champArray[championPosition];
+                        p.Image = Image.FromFile(@"..\..\obj\Images\" + champArray[championPosition] + ".png");
+                        p.SizeMode = PictureBoxSizeMode.StretchImage;
+                        nameOfGroup.Controls.Add(p);
+                        horizMargin = horizMargin + 120;
+
+                        championPosition = championPosition + 1;
+
+                        counter++;
+
+                        if (nameOfGroup.Name == "grpboxYourChampionPool")
+                            p.MouseClick += P_YourChampionPool_MouseClick;
+                        else if (nameOfGroup.Name == "grpboxChampionPool")
+                            p.MouseClick += P_ChampionPool_MouseClick;
+                        else
+                            MessageBox.Show("There was an error");
+                    }
+                }
+                vertMargin = vertMargin + 120;
+            }
+        }
+
+        private void P_YourChampionPool_MouseClick(object sender, MouseEventArgs e)
+        {
+            champSelectedFromYourPool += ((PictureBox)sender).Name + ",";
+            ((PictureBox)sender).Size = new Size(120, 120);
+            ((PictureBox)sender).BackColor = Color.LightBlue;
+            ((PictureBox)sender).Padding = new Padding(5);
+
+            MessageBox.Show(yourChampionPool);
+            yourChampionPool = yourChampionPool.Replace(((PictureBox)sender).Name + ",", "");
+            MessageBox.Show(yourChampionPool);
+            System.IO.File.WriteAllText(@"..\..\obj\TextFiles\YourChampionPool.txt", yourChampionPool);
+            FormClosed += (o, a) => new MainForm().ShowDialog();
+
+            Hide();
+            Close();
+        }
+
+        private void P_ChampionPool_MouseClick(object sender, MouseEventArgs e)
+        {
+            champSelectedFromPool += ((PictureBox)sender).Name + ",";
+            ((PictureBox)sender).Size = new Size(120, 120);
+            ((PictureBox)sender).BackColor = Color.LightBlue;
+            ((PictureBox)sender).Padding = new Padding(5);
+
+            MessageBox.Show(yourChampionPool);
+            yourChampionPool += ((PictureBox)sender).Name + ",";
+            MessageBox.Show(yourChampionPool);
+            System.IO.File.WriteAllText(@"..\..\obj\TextFiles\YourChampionPool.txt", yourChampionPool);
+            FormClosed += (o, a) => new MainForm().ShowDialog();
+
+            Hide();
+            Close();
+        }
+    }
+    //To do: Add button so that when you click the button it adds/removes all of the selected champions (create new method)
+    //to do this add champions to a string then add that string to the file
+    //Also, do a search to make sure you are not adding a champion twice.
+}
